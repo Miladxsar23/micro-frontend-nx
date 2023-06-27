87b73ffb294a689b76a9b2b3500844be11722c81
milad.xsar23 <milad.xsar72@gmail.com>
2023-06-27 09:44:29 +0330

    feat: post-management-services app was installed

---
+++ apps/services/post-managemenet-service/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'services-post-managemenet-service',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/services/post-managemenet-service',
+};
