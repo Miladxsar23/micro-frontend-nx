5e1addc8161dd06763ee4e281e6002438fbcfb63
milad.xsar23 <milad.xsar72@gmail.com>
2023-06-29 10:18:45 +0330

    fix: post-management-service is renamed

--- apps/services/post-management-service/jest.config.ts
+++ apps/services/post-management-service/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'services-post-managemenet-service',
+  displayName: 'services-post-management-service',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/apps/services/post-managemenet-service',
+  coverageDirectory: '../../../coverage/apps/services/post-management-service',
 };
