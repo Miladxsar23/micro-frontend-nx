7a0801386df4d9d576a094ed363a38ee99862f41
milad.xsar23 <milad.xsar72@gmail.com>
2023-06-30 09:03:19 +0330

    fix: controller logic changed

--- libs/post/utility/src/lib/services/postManagement.ts
+++ libs/post/utility/src/lib/services/postManagement.ts
@@ -4,6 +4,18 @@ import { Post } from '../entity';
 
 export class PostManagementService {
   private postRepository = dataSource.getMongoRepository(Post);
+  async getPosts(query: string | undefined): Promise<Post[]> {
+    let posts: Post[] = [];
+    if (query) {
+      // Find all records in the "post" table that match the query in title
+      posts = await this.postRepository.find({
+        where: { title: { $regex: new RegExp(query as string, 'i') } },
+      });
+    } else {
+      posts = await this.postRepository.find();
+    }
+    return posts;
+  }
   async getPost(postId: string): Promise<Post | undefined> {
     if (!ObjectId.isValid(postId)) return undefined;
     return this.postRepository.findOneBy({ _id: new ObjectId(postId) });
